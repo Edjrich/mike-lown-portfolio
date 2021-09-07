12969560a99d49329dd338cbdc940a8b30dd11ad
erich <ericdjrichardson@gmail.com>
2021-09-07 15:42:05 -0400

    redid hamburger menu. Now has animation, colour was changed to purple, background is now white. Added logo to menu, and changed some formatting in html. Needs work still, but is functioning correctly.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul')
+const navUL = document.getElementById('ham-ul')
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
-})
+    hamburger.classList.toggle('active')
+});
