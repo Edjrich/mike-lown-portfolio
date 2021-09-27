12e51bed0da9f2ca9d6fcef609064592f9da7bf3
eric-arches <eric@arches.dev>
2021-09-27 13:58:15 -0400

    major structural changs to make hamburger nav fixed.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('ham-ul')
+const navLogo = document.getElementById('nav-logo')
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
     hamburger.classList.toggle('active')
+    navLogo.classList.toggle('hide')
 });
